f325c3e5b646b36f965df8571db3a5475c3822c7
salomaoalg <sa.salomao@hotmail.com>
2023-01-17 15:59:22 -0300

    1008 uri

---
+++ 1008/index.js
@@ -0,0 +1,27 @@
+const { parse } = require("path");
+
+var input = require("fs").readFileSync("./1008/stdin", "utf8")
+var lines = input.split(" ");
+
+
+
+let N, SALARIO, VENDAS;
+
+N = lines.shift();
+SALARIO = parseFloat(lines.shift());
+VENDAS = parseFloat(lines.shift());
+
+console.log('TOTAL = R$', (SALARIO + ((VENDAS * 15)/ 100)).toFixed(2));
+
+
+
+
+//var PI = 3.14159;
+//var RAIO = parseFloat(lines.shift());
+//var AREA =  PI * Math.pow(RAIO, 2);
+
+//console.log('A=' + AREA.toFixed(4));
+
+//Comando para DEPURAÇÃO/EXECUÇÃO/TESTE
+//Lembrando do arquivo STDIN com slipt 
+// node .\1003\script.js
