b441eaf1b50707ab8c7fbb956c4b4806eb74bdc6
salomaoalg <sa.salomao@hotmail.com>
2023-04-12 13:56:51 -0300

    lógica feita
    terminar exibição em string sem espaços com dois pontos

--- 1019/index1019.js
+++ 1019/index1019.js
@@ -3,16 +3,28 @@ var lines = input.split("\n");
 
 let resto = 0; segundos = 0, horas = 0, minutos = 0; 
 segundos = parseInt(lines.shift());
+var tempo = [], cont = 0;
 
-
-while(segundos > 60){
+while(segundos >= 60){
     if (segundos >= 3600){
         horas = horas + 1;
-        console.log(horas);
+        segundos = segundos -3600;
     }
-    segundos = segundos - 1;
+    else{
+        minutos = minutos + 1;
+        segundos = segundos - 60;
+    }
+}
+tempo[0] = horas.toString();
+tempo[1] = minutos.toString();
+tempo[2] = segundos.toString();
+
+while(cont < tempo.length){
+    tempo = tempo.push(':')
+    cont = cont + 1; 
+    console.log(tempo[cont]);
 }
 
-segundos = segundos * 60
 
-console.log(horas, minutos, segundos);
+
+
