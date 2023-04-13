ba35e3d1e9f87a05f2869e1d3b48cef9e22a79d7
salomaoalg <sa.salomao@hotmail.com>
2023-04-13 14:06:55 -0300

    resolvido!!

--- 1020/index1020.js
+++ 1020/index1020.js
@@ -1,5 +1,26 @@
 var input = require("fs").readFileSync("./1020/stdin", "utf8")
 var lines = input.split("\n");
 
-segundos = parseInt(lines.shift());
+let dias = 0, ano = 0; mes = 0;
+dias = parseInt(lines.shift());
+
+while (dias > 0) {
+    if (dias >= 365) {
+        ano = ano + 1;
+        dias = dias - 365;
+    }
+    else if (dias >= 30) {
+        mes = mes + 1
+        dias = dias - 30;
+    }
+    else {
+        break;
+    }
+}
+
+console.log(ano,"ano(s)");
+console.log(mes,"mes(es)");
+console.log(dias,"dia(s)");
+
+
 
