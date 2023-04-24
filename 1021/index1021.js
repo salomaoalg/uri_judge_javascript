45e0c04c16f5289a18e321a7fe6ba3f19329e114
salomaoalg <sa.salomao@hotmail.com>
2023-04-24 16:07:07 -0300

    1021 resolvido funcionando

--- 1021/index1021.js
+++ 1021/index1021.js
@@ -1,12 +1,29 @@
 var input = require("fs").readFileSync("./1021/stdin", "utf8")
 var lines = input.split("\n");
-valor = 0;
 
+let cash = parseFloat(lines.shift());
+let contnotas = 0, contmoedas = 0;
 let notas = [100, 50, 20, 10, 5, 2];
-let moedas = [1, 0.50, 0,25, 0.10, 0.05, 0.01];
+let moedas = [1.00, 0.50, 0.25, 0.10, 0.05, 0.009];
 
+console.log("NOTAS:");
 notas.forEach(element => {
-    console.log(element); // O Elemento já é o índice!! 
+    while(cash >= element){
+        contnotas += 1; 
+        cash = cash - element;
+    }
+    console.log(contnotas,"nota(s) de R$",element.toFixed(2));
+    contnotas = 0;
+});
+console.log("MOEDAS:");
+moedas.forEach(element => {
+    while(cash >= element ){ // 0.01
+        contmoedas += 1; 
+        cash = cash - element;  
+    }
+    console.log(contmoedas,"moeda(s) de R$",element.toFixed(2));
+    //console.log("exibição do cash:", cash);
+    contmoedas = 0;
 });
 
 
