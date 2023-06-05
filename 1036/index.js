e3876c76a2e5d0544d04b958007d77619322e1ad
salomaoalg <sa.salomao@hotmail.com>
2023-06-05 13:21:35 -0300

    bhaskara

--- 1036/index.js
+++ 1036/index.js
@@ -5,6 +5,8 @@ let numeros, entrada, delta, A, B, C, X1, X2, X;
 entrada = lines.shift();
 numeros = entrada.split(" ").map(Number); 
 
+
+
 A = numeros[0];
 B = numeros[1];
 C = numeros[2];
@@ -12,14 +14,16 @@ C = numeros[2];
 if(C <= 0){
     C = Math.abs(C);
     delta = Math.pow(A,2) + ((4 * B) * C);
-    console.log(delta);
+    
 }
 else{
     delta = Math.pow(A,2) - ((4 * B) * C);
-    console.log(delta);
+    
 }
 
-X = 2 * A;
-X2 -B + Math.sqrt(delta)
+X1 =  (-B + Math.sqrt(delta)) / (2 * A);
+X2 = (-B - Math.sqrt(delta)) / (2 * A);
 
-
+console.log(delta);
+console.log(X1.toFixed(5));
+console.log(X2.toFixed(5));
